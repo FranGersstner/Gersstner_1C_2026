b830553d7150012ce18ce786a80d636029937e62
Francisco Gersstner <francisco.gersstner@ingenieria.uner.edu.ar>
2026-03-18 16:29:11 -0300

    Ejercicio 3 y 4 hechos
    Ejercicios 3 y 4 funcionando, de la guia 1

--- firmware/projects/Ej_3_guia_1/main/Ej_3_guia_1.c
+++ firmware/projects/Ej_3_guia_1/main/Ej_3_guia_1.c
@@ -26,14 +26,62 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
 /*==================[macros and definitions]=================================*/
-
 /*==================[internal data definition]===============================*/
 
+struct leds
+{
+    uint8_t mode;       //ON, OFF, TOGGLE
+	uint8_t n_led;      //indica el número de led a controlar
+	uint8_t n_ciclos;   //indica la cantidad de ciclos de encendido/apagado
+	uint16_t periodo;   //indica el tiempo de cada ciclo
+} my_leds; 
+
+enum {ON, OFF, TOGGLE};
+//#define OFF 0
+//#define ON 1			Otro modo	
+//#define TOGGLE 2
+
 /*==================[internal functions declaration]=========================*/
+void Control_Leds(struct leds *misleds) {
+	if (misleds == NULL) return;
+
+	switch (misleds->mode) {
+	
+	case ON:
+		LedOn(misleds->n_led);
+		break;
+	
+	case OFF:
+		LedOff(misleds->n_led);
+		break;
+	
+	case TOGGLE: {
+		uint8_t i = 0;
+		while (i < 2*misleds->n_ciclos) { //el 2*misleds->n_ciclos es porque estoy tomando a 1 ciclo como prender + apagar
+			LedToggle(misleds->n_led);
+			vTaskDelay(misleds->periodo/portTICK_PERIOD_MS);
+			i++;
+		}
+	break;
+	}
+
+	}
+}
+
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	LedsInit();
+	my_leds.mode = TOGGLE;
+	my_leds.n_ciclos = 5;
+	my_leds.n_led = LED_2;
+	my_leds.periodo = 500;
+	Control_Leds(&my_leds);
+
 }
 /*==================[end of file]============================================*/
--- firmware/projects/Ej_4_guia_1/main/Ej_4_guia_1.c
+++ firmware/projects/Ej_4_guia_1/main/Ej_4_guia_1.c
@@ -26,14 +26,32 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data definition]===============================*/
 
 /*==================[internal functions declaration]=========================*/
+int8_t convertToBcdArray(uint32_t data, uint8_t digits, uint8_t *bcd_number)
+{
+    if (bcd_number == NULL) return -1; //Esta linea es para hacer mas robusto el programa, Verifica que el puntero sea válido.
 
+    for (int i = digits - 1; i >= 0; i--) { //Recorre el array de derecha a izquierda
+        bcd_number[i] = data % 10;  // extrae el último dígito
+        data = data / 10;           // elimina ese dígito
+    }
+	printf("Numero en BCD: ");
+    for (int i = 0; i < digits; i++) {
+        printf("%d", bcd_number[i]);
+    }
+    printf("\n");
+    return 0;
+}
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	uint8_t bcd[5];  // arreglo donde se guardan los dígitos
+
+    convertToBcdArray(1234, 5, bcd);
 }
 /*==================[end of file]============================================*/
